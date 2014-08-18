29f93592a72265f56b55cfa5a0e091d9ffa318ae
JC Lavocat <jice@lavocat.name>
2014-08-18 13:40:48 +0200

    Added support for <link> tags

--- __init__.py
+++ __init__.py
@@ -66,7 +66,8 @@ class Phae(object):
             
         # Now that we have the html tree, we look for variations of author tag
         # In order rel="author" then rel="me"
-        list_author_urls = raw_html_tree.xpath("//a[@rel='me']/@href | //a[@rel='author']/@href ")
+        xpath_term = "//a[@rel='me']/@href | //a[@rel='author']/@href | //link[@rel='me']/@href | //link[@rel='author']/@href"
+        list_author_urls = raw_html_tree.xpath(xpath_term)
         # We then select only the first link found
         if len(list_author_urls)>0:
             first_author_url_rel = list_author_urls[0]
